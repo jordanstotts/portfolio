fac2f083cf8d53d9a3ba31dfa32e2b9930784470
jordanstotts <jordanstotts22@gmail.com>
2022-03-01 15:27:26 -0600

    add backend for resume pdf download

--- frontend/src/components/Resume.js
+++ frontend/src/components/Resume.js
@@ -1,14 +1,31 @@
 import React from "react";
 import Home from "./Home";
 import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+import fileDownload from "js-file-download";
 
 const Resume = () => {
+
+const download = (e) => {
+    e.preventDefault()
+    axios({
+        url:"http://localhost:4000",
+        method: "GET",
+        responseType:"blob"
+    }).then((res) => {
+        console.log(res)
+        fileDownload(res.data, "JordanStottsResume.pdf")
+    })
+    
+}
+
   return (
     <div>
       <Home />
       <p>Here is a link to download my updated resume!</p>
+      <div>create downloadable link to resume</div>
       <div>
-          create downloadable link to resume
+          <button onClick={(e) => download(e)} >Download</button>
       </div>
     </div>
   );
