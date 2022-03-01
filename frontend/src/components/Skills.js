c975c8882a27dd6d8223cb550a8731e8e700fccb
jordanstotts <jordanstotts22@gmail.com>
2022-03-01 13:47:36 -0600

    skills logos added

--- frontend/src/components/Skills.js
+++ frontend/src/components/Skills.js
@@ -2,31 +2,27 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./Home";
 
+
 const Skills = () => {
-  return (
+    return (
     <div>
       <Home />
       <p>Here are a few skills I've practiced over the past few months</p>
-      <div>
-          <h1>put images of skills here</h1>
-          <p>HTML</p>
-          <p>CSS</p>
-          <p>JavaScript</p>
-          <p>React</p>
-          <p>Bootstrap</p>
-          <p>Reactstrap</p>
-          <p>Node</p>
-          <p>Mongo</p>
-          <p>mongoDB</p>
-          <p>Express</p>
-          <p>Heroku</p>
-          <p>Python</p>
-          <p>Django</p>
-          <p>AWS</p>
-          <p>RESTful API's</p>
-          <p>PostgresSQL</p>
-          <p></p>
-      </div>
+          <div>
+              <img src="https://www.w3.org/html/logo/downloads/HTML5_Logo_256.png" alt="html"/>
+              <img src="https://cdn.iconscout.com/icon/free/png-256/css-37-226088.png" alt="css"/>
+              <img src="https://cdn.iconscout.com/icon/free/png-256/javascript-2038874-1720087.png" alt="javascript"/>
+              <img src="https://cdn.iconscout.com/icon/free/png-256/react-4-1175110.png" alt="react"/>
+              <img src="https://antman.info/images/news/logo.bootstrap.png" alt="bootstrap"/>
+              <img src="https://cdn.iconscout.com/icon/free/png-256/nodejs-2-226035.png" alt="node"/>
+              <img src="https://cdn.iconscout.com/icon/free/png-256/mongodb-5-1175140.png" alt="mongoDB"/>
+              <img src="https://cdn.iconscout.com/icon/free/png-256/express-8-1175029.png" alt="express"/>
+              <img src="https://cdn.iconscout.com/icon/free/png-256/heroku-225989.png" alt="heroku"/>
+              <img src="https://cdn.iconscout.com/icon/free/png-256/django-2-282855.png" alt="django"/>
+              <img src="https://cdn.iconscout.com/icon/free/png-256/aws-1869025-1583149.png" alt="aws"/>
+              <img src="https://s28309.pcdn.co/wp-content/themes/321-web-marketing/assets/images/api-logo-256.png" alt="rest api"/>
+              <img src="https://cdn.iconscout.com/icon/free/png-256/postgresql-11-1175122.png" alt="postgreSQL"/>
+          </div>
     </div>
   );
 };
